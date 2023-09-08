014233b5c0bac0697e06333369936cbea44518a5
roland-jurd <33280909+roland-jurd@users.noreply.github.com>
2023-09-08 14:37:45 +0200

    Update and rename example.spec.ts to example1.spec.ts

--- src/tests/example1.spec.ts
+++ src/tests/example1.spec.ts
@@ -1,13 +1,26 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+
+test('has title @smoke', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+
+test('get started link @sanity', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+
+test('get started link @regression', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
