700cf5d543a7929131975414524814a04fbbbd70
roland-jurd <33280909+roland-jurd@users.noreply.github.com>
2023-09-08 14:38:48 +0200

    Create example2.spec.ts

---
+++ src/tests/example2.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+
+test('has title 2 @smoke', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+
+test('get started link 2 @sanity', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
+
+
+test('get started link 2 @regression', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects the URL to contain intro.
+  await expect(page).toHaveURL(/.*intro/);
+});
