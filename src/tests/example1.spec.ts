1b0c703da08885e414ec584acccd98784e2c68bf
roland-jurd <33280909+roland-jurd@users.noreply.github.com>
2023-10-16 07:08:28 +0000

    Update example1.spec.ts

--- src/tests/example1.spec.ts
+++ src/tests/example1.spec.ts
@@ -4,7 +4,6 @@ import { test, expect } from '@playwright/test';
 test('has title @test1 @smoke', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
